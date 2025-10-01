a22371fad864ba625e2f1330c5ba9e33eebb6fa4
Emmanuel Chebukati <echebukati@heptanalytics.com>
2025-10-01 20:19:27 +0300

    kill drift

--- infra/azure/vnet.tf
+++ infra/azure/vnet.tf
@@ -25,14 +25,8 @@ resource "azurerm_subnet" "twingate_azure_container_subnet" {
 
   delegation {
     name = "delegation"
-
     service_delegation {
       name = "Microsoft.ContainerInstance/containerGroups"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/action",
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"
-      ]
     }
   }
 }
