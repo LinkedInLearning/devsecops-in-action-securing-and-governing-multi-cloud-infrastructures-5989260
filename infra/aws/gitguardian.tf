029e2ae87cc8da1ea46ed1d79c1d3dddadde1ac4
Emmanuel Chebukati <echebukati@heptanalytics.com>
2025-10-01 12:51:33 +0300

    add test secrets

---
+++ infra/aws/gitguardian.tf
@@ -0,0 +1,11 @@
+# variable "aws_access_key_id" {
+#   type      = string
+#   sensitive = true
+#   default   = "ASIA47FOVJDFJN4IHMTT"
+# }
+
+# variable "aws_secret_access_key" {
+#   type      = string
+#   sensitive = true
+#   default   = "wDYUXEfAwXEjhiJ2/DineDkhyTUavRXdcyNFVw7k"
+# }
---
+++ infra/azure/gitguardian.tf
@@ -0,0 +1,11 @@
+# variable "azure_client_id" {
+#   type      = string
+#   sensitive = true
+#   default   = "f95e669e-cf14-491f-891c-a165291fc488"
+# }
+
+# variable "azure_client_secret" {
+#   type      = string
+#   sensitive = true
+#   default   = "wFq8Q~xc2gNgGC62p8GEfeyGBccZYTVjZw_dtbPr"
+# }
