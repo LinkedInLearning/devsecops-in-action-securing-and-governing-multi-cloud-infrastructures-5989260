1d3c096379464661969c060e0f3933528ffb26f1
Emmanuel Chebukati <echebukati@heptanalytics.com>
2025-10-03 15:24:55 +0300

    cmmit

--- infra/aws/opa.tf
+++ infra/aws/opa.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "opa_aws" {
   instance_type = "t3.micro"
 
   tags = {
-    Name        = "HelloWorld"
+    Name        = "OPA AWS"
     Environment = "Sandbox"
     Project     = "Red30Tech"
   }
