030a4e90a24c3445d1e4d31ba55305b94bec8682
Emmanuel Chebukati <echebukati@gmail.com>
2025-10-05 21:55:23 +0300

    feat(app|infra): initial deployment (#81)
    * deploy api azure
    
    * deploy aws frontend

--- infra/aws/vpc.tf
+++ infra/aws/vpc.tf
@@ -1,13 +1,13 @@
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "5.21.0"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.21.0"
 
-#   name = "red30tech-vpc"
-#   cidr = "10.20.0.0/16"
+  name = "red30tech-vpc"
+  cidr = "10.20.0.0/16"
 
-#   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-#   private_subnets = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
-#   public_subnets  = ["10.20.101.0/24", "10.20.102.0/24", "10.20.103.0/24"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
+  public_subnets  = ["10.20.101.0/24", "10.20.102.0/24", "10.20.103.0/24"]
 
-#   enable_nat_gateway = false
-# }
+  enable_nat_gateway = false
+}
