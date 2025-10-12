856e0b2e4a42d1992de494619ce6c567485a7c6d
Emmanuel Chebukati <echebukati@heptanalytics.com>
2025-10-12 20:01:29 +0300

    enable doppler

--- infra/azure/doppler.tf
+++ infra/azure/doppler.tf
@@ -1,17 +1,17 @@
-# resource "doppler_project" "backend" {
-#   name        = "red30tech-backend"
-#   description = "Backend app secrets"
-# }
+resource "doppler_project" "backend" {
+  name        = "red30tech-backend"
+  description = "Backend app secrets"
+}
 
-# resource "doppler_environment" "dev" {
-#   project = doppler_project.backend.name
-#   name    = "Development"
-#   slug    = "dev"
-# }
+resource "doppler_environment" "dev" {
+  project = doppler_project.backend.name
+  name    = "Development"
+  slug    = "dev"
+}
 
-# resource "doppler_service_token" "backend_dev_azure" {
-#   project = doppler_project.backend.name
-#   config  = doppler_environment.dev.slug
-#   name    = "backend-dev-azure-readonly"
-#   access  = "read"
-# }
+resource "doppler_service_token" "backend_dev_azure" {
+  project = doppler_project.backend.name
+  config  = doppler_environment.dev.slug
+  name    = "backend-dev-azure-readonly"
+  access  = "read"
+}
